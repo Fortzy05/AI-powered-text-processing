ea3af887221b56e037013ef378bf9e25265b1f1f
Fortzy05 <omorodion.fortune@gmail.com>
2025-02-17 16:06:45 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-[100vh] justify-center items-center text-5xl">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
